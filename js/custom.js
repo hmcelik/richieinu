088c634d865d767a3648fb7e6a2056ae4d9929c6
huseyinmelihcelik <huseyinmelihcelik@gmail.com>
2022-04-30 11:45:25 +0300

    i

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+(function($) {
+    $(document).ready(function () {
+        $(document).on('click', '.mfp-close', function () {
+            $('.popup-sh').remove()
+        })
+    })
+})(jQuery);
